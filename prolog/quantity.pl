c094ffa6cc2bcbc9f60bbbbdd4d3dea043956bfd
mgondan <Matthias.Gondan@psy.ku.dk>
2020-03-29 11:47:23 +0200

    Update quantity.pl

--- prolog/quantity.pl
+++ prolog/quantity.pl
@@ -1,230 +1,430 @@
 :- module(quantity, [
     quantity/3, 
-    match_quantities/3]).
+    match/3]).
     
 :- use_module(library(dcg/basics)).
 
-%
-% General numeric input like "1.5 cm"
-%
-quantity(String, Number, Options) :-
-    ground(String),
-    !, string_codes(String, Codes), 
-    quantity(Number, Options, Codes, []).
+:- use_module(library(dcg/basics)).
 
-quantity(String, Number, Options) :-
-    ground(Number), 
-    !, ( option(dec(D), Options) -> format(atom(Mask), '~~~wf', D) ; Mask = '~g' ),
-    format(string(String), Mask, Number).
+:- discontiguous qex/0.
+:- discontiguous quant/3.
+:- discontiguous from//1.
+:- discontiguous to//1.
+:- discontiguous places/3.
 
-match_quantities(Solution, Response, []) :-
-    quantity(Solution, Number_sol, Options_sol),
-    option(dec(Dec), Options_sol),
-    quantity(Response, Number_res, Options_res),
-    option(dec(Dec), Options_res),
-    Number_sol = Number_res.
-    
-% match_quantities(Solution, Response, [buggy(dec(Response) \= Sol, FB)]) :-
-match_quantities(Solution, Response, [FB]) :-
-    quantity(Solution, Number_sol, Options_sol),
-    option(dec(Sol), Options_sol),
-    quantity(Response, Number_res, Options_res),
-    option(dec(Res), Options_res),
-    Sol \= Res,
-    N is round(Number_sol * 10^min(Sol, Res)) / 10^min(Sol, Res),
-    N is round(Number_res * 10^min(Sol, Res)) / 10^min(Sol, Res),
-    FB = ["Please report the result with ", Sol, " decimal place(s)."].
-    
-%
-% 1.5E10
-%
-quantity(Number, Options) --> 
-    number(Number, Options).
+% Match two terms
+match(Ref, Input, Diff) :-
+    quantity(R, ROpt, Ref),
+    quantity(I, IOpt, Input),
+    match(R, ROpt, I, IOpt, Diff).
 
-%
-% 10 kg
-%
-quantity(Number, Options) --> 
-    number(Num, NumOpt),
-    blanks,
-    unit(_Unit, UnitOpt),
-    { option(fac(Kilo), UnitOpt, 1),
-      Number is Num * Kilo, 
-      option(dec(D), NumOpt, 0),
-      Dec is D - log10(Kilo),
-      merge_options([dec(Dec) | UnitOpt], NumOpt, Options)
-    }.
+% Parse quantity
+quantity(Term, String, Options) :-
+    string(String),
+    string_codes(String, Codes),
+    quant(Term, Codes, Options).
 
-% Real number
-number(Number, Options) -->
-    intdotfrac(Int, IntOpt),
-    power(Pow),
-    { Number is Int * 10^Pow, 
-      option(dec(D), IntOpt, 0),
-      Dec is D - Pow,
-      merge_options([dec(Dec)], IntOpt, Options) 
-    }.
+quantity(Term, Codes, Options) :-
+    quant(Term, Codes, Options).
 
-% sign(?Number, ?Options, ?Codes)//
-sign(+1, [sign(+)]) --> 
+% Internal
+match(Ref, ROpt, Input, IOpt, Diff) :-
+    option(places(Default), IOpt, 2),
+    option(places(Places), ROpt, Default),
+    round(Ref * 10^Places) =:= round(Input * 10^Places),
+    diff(ROpt, IOpt, Diff).
+
+match(Ref, ROpt, Input, IOpt, Diff) :-
+    option(places(Default), IOpt, 2),
+    option(places(Places), ROpt, Default),
+    Default =:= Places - 1,
+    round(Ref * 10^Default) =:= round(Input * 10^Default),
+    diff(ROpt, IOpt, Diff).
+
+diff(Ref, Input, []) :-
+    option(places(P), Ref),
+    option(places(P), Input, P).
+
+diff(Ref, Input, ["Please report the result with ", P, " decimal places."]) :-
+    option(places(P), Ref),
+    option(places(Q), Input, P),
+    P =\= Q.
+
+% Term from codes
+from(Term, Codes) :-
+    format("~s", [Codes]),
+    call(Term, Codes, [])
+    -> format(" -> ~k~n", Term)
+     ; format(": conversion failed~n").
+
+qex(Codes) :-
+    format("~s", [Codes]),
+    quant(Term, Options, Codes),
+    format(" -> ~k ~k~n", [Term, Options]).
+
+% Term to codes
+to(Term, Codes) :-
+    format("~k", Term),
+    call(Term, Codes, [])
+    -> format(" -> ~s~n", [Codes])
+     ; format(": conversion failed~n").
+
+% Natural number
+quant(Term, [type(natural)], Codes) :-
+    natural(Term, Codes, []).
+
+natural(N) -->
+    { ground(N) }
+    -> to(natural(N))
+     ; from(natural(N)).
+
+% Term to codes
+to(natural(N)) -->
+    { number_codes(N, Codes) },
+    digits(Codes).
+
+% Term from codes
+from(natural(N)) -->
+    digits([C | Codes]),
+    { number_codes(N, [C | Codes]) }.
+
+qex :- from(natural(_N), `123`).
+qex :- qex(`123`).
+qex :- to(natural(123), _Codes).
+
+% Integer number
+quant(Term, [type(integer) | Options], Codes) :-
+    integ(Term, Options, Codes, []).
+
+integ(I, Options) -->
+    { ground(I) }
+    -> to(integer(I, Options))
+     ; from(integer(I, Options)).
+
+to(integer(I, Options)) -->
+    { S is integer(sign(I)),
+      N is integer(abs(I))
+    },
+    sign(S, Options),
+    natural(N).
+
+from(integer(I, Options)) -->
+    sign(S, Options),
+    natural(N),
+    { I is S * N }.
+
+sign(S, Options) -->
+    { ground(S) }
+    -> to(sign(S, Options))
+     ; from(sign(S, Options)).
+
+to(sign(1, Options)) -->
+    { option(sign(pretty), Options, default) },
+    "".
+
+to(sign(1, Options)) -->
+    { option(sign(default), Options, default) },
     "+".
 
-sign(-1, [sign(-)]) --> 
-    "-".
+to(sign(0, _)) -->
+    "".
 
-sign(-1, [sign(-)]) --> 
+to(sign(-1, Options)) -->
+    { option(sign(pretty), Options, default) },
     [226, 136, 146].
 
-sign(1, [sign(none)]) --> 
-    [].
+to(sign(-1, Options)) -->
+    { option(sign(default), Options, default) },
+    "-".
 
-% Natural number nat(?Number)//
-nat(Number) -->
-    { ground(Number),
-      number_codes(Number, Dig)
-    }, !, digits(Dig).
+from(sign(1, Options)) -->
+    { memberchk(sign(pretty), Options) },
+    "".
 
-nat(Number) -->
-    digits([H | T]),
-    { number_codes(Number, [H | T]) }.
+from(sign(1, Options)) -->
+    { memberchk(sign(default), Options) },
+    "+".
 
-% Decimal separator dot//
-dot --> ".".
-dot --> ",".
+from(sign(-1, Options)) -->
+    { memberchk(sign(pretty), Options) },
+    [226, 136, 146].
 
-% Fractional part frac(?Num, ?Options)//
-frac(Number, [dec(Dec)]) -->
-    { ground(Number),
-      Num is round(Number * 10.0^Dec),
-      number_codes(Num, Dig)
-    }, !, digits(Dig).
+from(sign(-1, Options)) -->
+    { memberchk(sign(default), Options) },
+    "-".
 
-frac(Number, [dec(Dec)]) -->
-    digits([H | T]),
-    { number_codes(Num, [H | T]),
-      length([H | T], Dec),
-      Number is Num / 10.0^Dec
+qex :- from(integ(_N, _Opt), `123`).
+qex :- qex(`123`).
+qex :- to(integ(123, [sign(pretty)]), _Codes).
+
+qex :- from(integ(_N, _Opt), `-123`).
+qex :- qex(`-123`).
+qex :- to(integ(-123, [sign(pretty)]), _Codes).
+
+% Real number
+quant(Term, [type(real) | Options], Codes) :-
+    real(Term, Options, Codes, []).
+
+real(R, Options) -->
+    { ground(R) }
+    -> to(real(R, Options))
+     ; from(real(R, Options)).
+
+to(real(R, Options)) -->
+    { I is float_integer_part(R),
+      F is float_fractional_part(abs(R))
+    },
+    integ(I, Options),
+    comma(R, Options),
+    places(F, Options).
+
+from(real(R, Options)) -->
+    integ(I, Options),
+    comma(_, Options),
+    places(F, Options),
+    { R is I + sign(I) * F }.
+
+comma(R, Options) -->
+    { ground(R) }
+    -> to(comma(R, Options))
+     ; from(comma(R, Options)).
+
+to(comma(R, Options)) -->
+    { option(unit(Unit), Options, none),
+      places(R, Unit, Default),
+      option(places(0), Options, Default)
+    },
+    "".
+
+to(comma(R, Options)) -->
+    { option(unit(Unit), Options, none),
+      places(R, Unit, Default),
+      option(places(Places), Options, Default),
+      Places > 0,
+      option(dec(.), Options, .)
+    },
+    ".".
+
+to(comma(_, Options)) -->
+    { option(places(Places), Options, 2),
+      Places > 0,
+      option(dec(,), Options)
+    },
+    ",".
+
+from(comma(_, Options)) -->
+    { memberchk(places(0), Options) },
+    "".
+
+from(comma(_, Options)) -->
+    { memberchk(dec(.), Options) },
+    ".".
+
+from(comma(_, Options)) -->
+    { memberchk(dec(,), Options) },
+    ",".
+
+places(F, Options) -->
+    { ground(F) }
+    -> to(places(F, Options))
+     ; from(places(F, Options)).
+
+to(places(F, Options)) -->
+    { option(unit(Unit), Options, none),
+      places(F, Unit, Default),
+      option(places(0), Options, Default)
+    },
+    "".
+
+to(places(F, Options)) -->
+    { option(unit(Unit), Options, none),
+      places(F, Unit, Default),
+      option(places(Places), Options, Default),
+      Places > 0,
+      format(atom(Mask), '~~`0t~~d~~~w+', Places),
+      format(codes(Codes), Mask, round(F * 10^Places))
+    },
+    digits(Codes).
+
+from(places(0, Options)) -->
+    { memberchk(places(0), Options) },
+    "".
+
+from(places(F, Options)) -->
+    { memberchk(dec(_), Options) },
+    digits([C | Codes]),
+    { number_codes(N, [C | Codes]),
+      length([C | Codes], Places),
+      F is N / 10^Places,
+      memberchk(places(Places), Options)
     }.
 
-% 15
-intdotfrac(Number, [dec(0) | Options]) -->
-    sign(Sign, Options),
-    nat(Int),
-    { Number is Sign * Int }.
-    
-% 15.5
-intdotfrac(Number, Options) -->
-    sign(Sign, SgnOpt),
-    nat(Int),
-    dot,
-    frac(Frac, FracOpt),
-    { Number is Sign * (Int + Frac),
-      merge_options(SgnOpt, FracOpt, Options)
-    }.
+qex :- from(real(_N, _Opt), `12`).
+qex :- qex(`12`).
+qex :- to(real(12, []), _Codes).
 
-% .70
-intdotfrac(Number, Options) -->
-    sign(Sign, SgnOpt),
-    dot,
-    frac(Frac, FracOpt),
-    { Number is Sign * Frac,
-      merge_options(SgnOpt, [int(empty) | FracOpt], Options)
-    }.
-    
-% times 10^E
-power(Pow) --> 
-    exp_e, 
-    sign(Sign, _),
-    nat(P),
-    { Pow is Sign * P }.
-    
-power(0) --> [].
+qex :- from(real(_N, _Opt), `12.3`).
+qex :- qex(`12.3`).
+qex :- to(real(12.3, [places(1)]), _Codes).
 
-exp_e --> "E".
-exp_e --> "e".
-exp_e --> blanks, "*", blanks, "10", blanks, "^", blanks.
-exp_e --> blanks, "*", blanks, "10", blanks, "**", blanks.
-exp_e --> blanks, [195, 151], blanks, "10", blanks, "^", blanks.
-exp_e --> blanks, [195, 151], blanks, "10", blanks, "**", blanks.
+qex :- from(real(_N, _Opt), `-12.3`).
+qex :- qex(`-12.3`).
+qex :- to(real(-12.3, []), _Codes).
 
-unit('%', [fac(0.01), si('%')]) --> "%".
+%
+% Number with unit
+%
+quant(Term, [type(measure) | Options], Codes) :-
+    measure(Term, Options, Codes, []).
 
-unit(U, [si(SI) | Options]) --> 
-    modifier(M, Options), 
-    si(SI),
-    { string_concat(M, SI, U) }.
+measure(R, Options) -->
+    { ground(R) }
+    -> to(measure(R, Options))
+     ; from(measure(R, Options)).
 
-% Modifier
-modifier(k, [fac(1000)]) --> "k".
-modifier(c, [fac(0.01)]) --> "c".
-modifier(m, [fac(0.001)]) --> "m".
-modifier('', [fac(1)]) --> "".
+to(measure(R, Options)) -->
+    real(R, Options),
+    unit(R, Options).
 
-% Unit
-si(g) --> "g".
-si(m) --> "m".
+from(measure(R, Options)) -->
+    real(R, Options),
+    unit(R, Options).
 
-ex_quant :-
-    S = ".5 kg", 
-    quantity(S, N, Options),
-    writeln(S), writeln(N), writeln(Options).
+unit(R, Options) -->
+    { ground(R) }
+    -> to(unit(R, Options))
+     ; from(unit(R, Options)).
 
-ex_quant :-
-    S = "1.5 kg",
-    quantity(S, N, Options),
-    writeln(S), writeln(N), writeln(Options).
+places(_, none, 2).
+places(_, '%', 0).
+places(_, m, 2).
+places(_, kg, 1).
 
-ex_quant :-
-    S = "15 kg",
-    quantity(S, N, Options),
-    writeln(S), writeln(N), writeln(Options).
+to(unit(_, Options)) -->
+    { option(unit('%'), Options) },
+    "%".
 
-ex_quant :-
-    S = "15 g",
-    quantity(S, N, Options),
-    writeln(S), writeln(N), writeln(Options).
+to(unit(_, Options)) -->
+    { option(unit(m), Options) },
+    " ",
+    "m".
 
-ex_quant :-
-    S = "-15 g",
-    quantity(S, N, Options),
-    writeln(S), writeln(N), writeln(Options).
+to(unit(_, Options)) -->
+    { option(unit(kg), Options) },
+    " ",
+    "kg".
 
-ex_quant :-
-    S = "-15E10 g",
-    quantity(S, N, Options),
-    writeln(S), writeln(N), writeln(Options).
+from(unit(_, Options)) -->
+    blanks, "%",
+    { memberchk(unit('%'), Options) }.
 
-ex_quant :-
-    S = "-1.5E-10 g",
-    quantity(S, N, Options),
-    writeln(S), writeln(N), writeln(Options).
+from(unit(_, Options)) -->
+    blanks, "m",
+    { memberchk(unit(m), Options) }.
 
-ex_quant :-
-    S = "0.09",
-    quantity(S, N, Options),
-    writeln(S), writeln(N), writeln(Options).
+from(unit(_, Options)) -->
+    blanks, "kg",
+    { memberchk(unit(kg), Options) }.
 
-ex_quant :-
-    S = "9%",
-    quantity(S, N, Options),
-    writeln(S), writeln(N), writeln(Options).
+qex :- from(measure(_N, _Opt), `50%`).
+qex :- qex(`50%`).
+qex :- to(measure(50, [unit('%')]), _Codes).
 
-ex_quant :-
-    S = "9.1 %",
-    quantity(S, N, Options),
-    writeln(S), writeln(N), writeln(Options).
+qex :- from(measure(_N, _Opt), `12.3 kg`).
+qex :- qex(`12.3 kg`).
+qex :- to(measure(12.3, [unit(kg)]), _Codes).
 
-ex_quant :-
-    Sol = "3.14", Res = "3.141",
-    match_quantities(Sol, Res, Comment),
-    writeln(Sol), writeln(Res), writeln(Comment).
+%
+% Statistic
+%
+quant(Term, [type(statistic) | Options], Codes) :-
+    statistic(Term, Options, Codes, []).
 
-ex_quant :-
-    Sol = "3.141", Res = "3.14",
-    match_quantities(Sol, Res, Comment),
-    writeln(Sol), writeln(Res), writeln(Comment).
+statistic(S, Options) -->
+    { ground(S) }
+    -> to(statistic(S, Options))
+     ; from(statistic(S, Options)).
 
-ex_quant :-
-    Sol = "3.14", Res = "3.14",
-    match_quantities(Sol, Res, Comment),
-    writeln(Sol), writeln(Res), writeln(Comment).
+to(statistic(S, Options)) -->
+    { option(ratio(Unit), Options, none),
+      places(S, Unit, Default),
+      option(places(Places), Options, Default)
+    },
+    ratio(S, Options),
+    " ",
+    equals(S),
+    " ",
+    real(S, [sign(pretty), places(Places) | Options]).
+
+from(statistic(S, Options)) -->
+    ratio(S, Options),
+    blanks,
+    equals(S),
+    blanks,
+    real(S, Options).
+
+equals(_) -->
+    "=".
+
+ratio(S, Options) -->
+    { ground(S) }
+    -> to(ratio(S, Options))
+     ; from(ratio(S, Options)).
+
+to(ratio(_, Options)) -->
+    { option(ratio(z), Options)
+    },
+    "z".
+
+from(ratio(_, Options)) -->
+     "z",
+     { memberchk(ratio(z), Options) }.
+
+places(Z, z, 2) :-
+    abs(Z) < 10.
+
+places(Z, z, 1) :-
+    abs(Z) >= 10.
+
+to(ratio(_, Options)) -->
+    { option(ratio(t(Df)), Options) },
+    "t(", number(Df), ")".
+
+from(ratio(_, Options)) -->
+    "t(", number(Df), ")",
+     { memberchk(ratio(t(Df)), Options) }.
+
+places(T, t(_), 2) :-
+    abs(T) < 10.
+
+places(T, t(_), 1) :-
+    abs(T) >= 10.
+
+to(ratio(_, Options)) -->
+    { option(ratio('F'(Df1, Df2)), Options) },
+    "F(", number(Df1), ", ", number(Df2), ")".
+
+from(ratio(_, Options)) -->
+    "F(", number(Df1), ",", blanks, number(Df2), ")",
+     { memberchk(ratio('F'(Df1, Df2)), Options) }.
+
+places(F, 'F'(_, _), 2) :-
+    abs(F) < 10.
+
+places(F, 'F'(_, _), 1) :-
+    abs(F) >= 10.
+
+qex :- from(statistic(_N, _Opt), `z = 1.96`).
+qex :- qex(`z = 1.96`).
+qex :- to(statistic(1.9, [ratio(z)]), _Codes).
+qex :- to(statistic(19.61, [ratio(z)]), _Codes).
+
+qex :- from(statistic(_N, _Opt), `t(15) = 2.12`).
+qex :- qex(`t(15) = 2.12`).
+qex :- to(statistic(2.1, [ratio(t(15))]), _Codes).
+qex :- to(statistic(21.21, [ratio(t(15))]), _Codes).
+
+qex :- from(statistic(_N, _Opt), `F(10, 150) = 12.12`).
+qex :- qex(`F(10, 150) = 12.12`).
+qex :- to(statistic(2.1, [ratio('F'(10, 150))]), _Codes).
+qex :- to(statistic(12.12, [ratio('F'(10, 150))]), _Codes).
